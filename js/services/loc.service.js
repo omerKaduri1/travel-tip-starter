649ed76657c58952a3fb267cc684666c39ba35b1
LeanneBoro <leanneboro@gmail.com>
2024-03-06 16:48:23 +0200

    Add filter by creation time

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -54,8 +54,9 @@ function query() {
                 locs.sort((p1, p2) => (p1.rate - p2.rate) * gSortBy.rate)
             } else if (gSortBy.name !== undefined) {
                 locs.sort((p1, p2) => p1.name.localeCompare(p2.name) * gSortBy.name)
+            }else if (gSortBy.date !== undefined) {
+                locs.sort((p1, p2) => new Date(p1.createdAt) - new Date(p2.createdAt) * gSortBy.date)
             }
-
             return locs
         })
 }
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -9,7 +9,7 @@ export const mapService = {
 }
 
 // TODO: Enter your API Key
-const API_KEY = ''
+const API_KEY = 'AIzaSyB63mOlanpc7Mdu8zQ7mLwN_a4VPCZirFs'
 var gMap
 var gMarker
 
